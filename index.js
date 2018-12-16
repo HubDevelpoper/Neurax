d9ad360208791b7a77051e696ca77d0d726ca6b2
Fury <45101096+Black-Fury@users.noreply.github.com>
2018-12-16 20:33:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -109,6 +109,14 @@ if(message.content.startsWith("Wink"))
 message.react("519140915558744064");
 
 	
+else if (command === 'args-info') {
+    if (!args.length) {
+        return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+    }
+
+    message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+}
+	
 	
 	
 	
